9efe7d7b7dc66d216ec28c4fcec5ad7170a370c4
Felipe Pardo Suarez <felipe.pardosuarez@gmail.com>
2017-02-18 02:57:57 +0000

    review forms save to db and render!

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,20 @@
+class ReviewsController < ApplicationController
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    @review.user = current_user
+    if @review.save
+      redirect_to [@product]
+    else
+      redirect_to [@product]
+    end
+  end
+
+private
+
+  def review_params
+    params.require(:review).permit(:description, :rating)
+  end
+
+end
