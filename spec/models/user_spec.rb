1387474c787fc67f43cc827572eedf2ff1a5bda6
Felipe Pardo Suarez <felipe.pardosuarez@gmail.com>
2017-02-21 00:15:07 +0000

    test for min length of password to be 6

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,37 +13,36 @@ RSpec.describe User, type: :model do
     expect(@user.password.present? && @user.password_confirmation.present?).to be true
   end
 
-  it 'should have matching password and password_confirmation' do
+  it 'should not be created if password and password_confirmation do not match' do
     @user = User.create(
       email: 'test@example.com',
       password: 'password123',
-      password_confirmation: 'password123',
+      password_confirmation: 'not_the_same',
       first_name: 'name',
       last_name: 'lastname'
     )
-    expect(@user.password).to eql(@user.password_confirmation)
+    expect(@user.save).to be false
   end
 
-  it "should be created with an email" do
+  it "should not be created if email is not present" do
     @user = User.create(
-      email: 'test@example.com',
+      email: nil,
       password: 'password123',
       password_confirmation: 'password123',
       first_name: 'name',
       last_name: 'lastname'
     )
-    expect(@user.email.present?).to be true
+    expect(@user.save).to be false
   end
 
   context "given that emails are the same" do
     it "should not save a user with an email already in use" do
       @user_one = User.create(
-        email: 'one@example.com',
+        email: 'ONE@example.com',
         password: 'password123',
         password_confirmation: 'password123',
         first_name: 'name',
         last_name: 'lastname'
-
       )
       @user_two = User.create(
         email: 'one@example.COM',
@@ -56,25 +55,37 @@ RSpec.describe User, type: :model do
     end
   end
 
-  it "should be created with a first_name" do
+  it "should not be created if first_name is not present" do
     @user = User.create(
       email: 'test@example.com',
       password: 'password123',
       password_confirmation: 'password123',
-      first_name: 'name',
+      first_name: nil,
       last_name: 'lastname'
     )
-    expect(@user.first_name.present?).to be true
+    expect(@user.save).to be false
   end
 
-  it "should be created with a last_name" do
+  it "should not be created if last_name is not present" do
     @user = User.create(
       email: 'test@example.com',
       password: 'password123',
       password_confirmation: 'password123',
       first_name: 'name',
+      last_name: nil
+    )
+    expect(@user.save).to be false
+  end
+
+  it "should not be created if password.length is less than 6" do
+    @user = User.create(
+      email: 'test@example.com',
+      password: 'pass',
+      password_confirmation: 'pass',
+      first_name: 'name',
       last_name: 'lastname'
     )
-    expect(@user.last_name.present?).to be true
+    expect(@user.save).to be false
   end
+
 end
